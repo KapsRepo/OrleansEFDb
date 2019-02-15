5eabb92d1982323e9364087102b164636c0b034d
Michael Kaboro <michael.kaboro@gmail.com>
2019-02-15 05:00:20 +0300

    Add project files.

---
+++ OrleansEFDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrleansEFDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
